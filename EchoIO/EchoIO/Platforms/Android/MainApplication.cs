4294714da012474cba6d7cdadfc9265732603252
Dimitrios Papageorgacopoulos <35904643+DimitriosPt@users.noreply.github.com>
2022-09-18 12:58:43 -0700

    Additional warning fixes

--- EchoIO/EchoIO/Platforms/Android/MainApplication.cs
+++ EchoIO/EchoIO/Platforms/Android/MainApplication.cs
@@ -2,18 +2,30 @@
 // Copyright (c) DimitriosPapageorgacopoulos. All rights reserved.
 // </copyright>
 
+namespace EchoIO;
+
 using Android.App;
 using Android.Runtime;
 
-namespace EchoIO;
-
+/// <summary>
+/// Launches the maui application.
+/// </summary>
 [Application]
 public class MainApplication : MauiApplication
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MainApplication"/> class.
+    /// </summary>
+    /// <param name="handle">Memory address handle.</param>
+    /// <param name="ownership">Handle ownership.</param>
     public MainApplication(IntPtr handle, JniHandleOwnership ownership)
         : base(handle, ownership)
     {
     }
 
+    /// <summary>
+    /// Creates the maui application.
+    /// </summary>
+    /// <returns>The new application that was created.</returns>
     protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
--- EchoIO/EchoIO/Platforms/iOS/Program.cs
+++ EchoIO/EchoIO/Platforms/iOS/Program.cs
@@ -2,15 +2,21 @@
 // Copyright (c) DimitriosPapageorgacopoulos. All rights reserved.
 // </copyright>
 
+namespace EchoIO;
+
 using ObjCRuntime;
 using UIKit;
 
-namespace EchoIO;
-
+/// <summary>
+/// The primary entry point for the application.
+/// </summary>
 public class Program
 {
-    // This is the main entry point of the application.
-    static void Main(string[] args)
+    /// <summary>
+    /// Makes a call to initialize the UI.
+    /// </summary>
+    /// <param name="args">Additional runtime arguments.</param>
+    internal static void Main(string[] args)
     {
         // if you want to use a different Application Delegate class from "AppDelegate"
         // you can specify it here.
